4fd4e11989ae32027ab4ab825ed8497d1f252d61
ipaulaandreea <andreeapaulaivan@gmail.com>
2026-05-11 17:37:13 +0300

    Command

---
+++ CTS/Command/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Light light = new Light();
+
+        Command onCommand = new TurnOnCommand(light);
+        Command offCommand = new TurnOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(onCommand);
+        remote.pressButton();
+
+        remote.setCommand(offCommand);
+        remote.pressButton();
+    }
+}
